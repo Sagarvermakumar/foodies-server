ecb3c366f2b6902ced927b5a14a413c5d0e6810a
SAGAR VERMA <93580397+Sagarvermakumar@users.noreply.github.com>
2025-09-17 08:44:56 +0530

    fix(cors): enable credentials and update allowed origins

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const allowedOrigins = [
   "https://zayka-express-six.vercel.app",
   "https://zayka-nu.vercel.app"
 ];
-
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -26,9 +25,16 @@ app.use(
     },
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"], // ✅ FIXED as array
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+// sending cookies/token with credentials
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());

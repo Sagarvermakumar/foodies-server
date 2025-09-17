fbf26a16ff95ad1d8669804277de88a0ff053499
SAGAR VERMA <93580397+Sagarvermakumar@users.noreply.github.com>
2025-09-17 07:47:07 +0530

    fix(db): correct database name in config

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     const MONGO_URI = config.NODE_ENV === "production" ? config.MONGO_URI_CLOUD : config.MONGO_URI_LOCAL
 
     const connection = await mongoose.connect(MONGO_URI,{
-      dbName:"Yumz"
+      dbName:"food-delivery-system"
     });
     console.log(`Database connected to ${connection.connection.host}`);
   } catch (error) {

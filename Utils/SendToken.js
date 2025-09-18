16b957eb06c6ef18c2dd35b1ecaacf9eb58a3088
sagarvermakumar <sksagarkumarverma@gmail.com>
2025-09-18 09:31:57 +0530

    fixing token issu

--- Utils/SendToken.js
+++ Utils/SendToken.js
@@ -1,22 +1,18 @@
 export const sendToken = (res, user, message, statusCode = 200) => {
-  const token = user.getJWTToken();
-
-  const isProduction = process.env.NODE_ENV === "production";
-
-  const cookieOptions = {
-    httpOnly: true,
-    expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
-    secure: isProduction,
-    sameSite: isProduction ? "none" : "lax",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-  };
-
+  const token = user.getJWTToken()
+console.log("token : ", token)
+  const isProduction = process.env.NODE_ENV === 'production'
   res
     .status(statusCode)
-    .cookie("token", token, cookieOptions)
+    .cookie('token', token, {
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: isProduction ? 'none' : 'lax',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    })
     .json({
       success: true,
       message,
       user,
-    });
-};
+    })
+}

7644e0a5bf30c681ce605f6f12928fe036b71b8d
sagarvermakumar <sksagarkumarverma@gmail.com>
2025-09-18 09:50:24 +0530

    fix(auth): set SameSite=None for cross-site cookies

--- Utils/SendToken.js
+++ Utils/SendToken.js
@@ -7,7 +7,7 @@ console.log("token : ", token)
     .cookie('token', token, {
       httpOnly: true,
       secure: isProduction,
-      sameSite: isProduction ? 'none' : 'lax',
+      sameSite: isProduction ? 'None' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
     .json({
